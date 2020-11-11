5641838a43ee9a72449f35880036613089548f6b
Pu Wang <nicolas.weeks@gmail.com>
2020-11-11 13:12:17 +0000

    fix travis ci

--- build/test_in_kata.sh
+++ build/test_in_kata.sh
@@ -1,5 +1,15 @@
 #! /bin/sh
-set -exu
+set -o errexit
+set -o nounset
+set -o xtrace
+
+ADD_CARGO_REPO_MIRROR=${ADD_CARGO_REPO_MIRROR:-"true"}
+BCC=${BCC:-"0.17.0"}
+#BUILD_BCC_INSIDE_KATA=${BUILD_BCC_INSIDE_KATA:-false}
+FEATURES=${FEATURES:-"v0_17_0"}
+LLVM=${LLVM:-"9"}
+WORK_DIR=${WORK_DIR:-"."}
+STATIC=${STATIC:-"true"}
 
 # debugfs is used by tracepoints in BPF
 MOUNTED=`cat /proc/self/mountinfo | grep debugfs | awk '{print $5}'`
@@ -9,39 +19,38 @@ else
     echo "debugfs mounted at $MOUNTED"
 fi
 
-BCC=${BCC:-"0.17.0"}
-BUILD_BCC_INSIDE_KATA=${BUILD_BCC_INSIDE_KATA:-false}
-FEATURES=${FEATURES:-"v0_17_0"}
-LLVM=${LLVM:-"9"}
-WORK_DIR=${WORK_DIR:-"."}
-STATIC=${STATIC:-"true"}
-
 # Build and install BCC
 cd $WORK_DIR
-if [ $BUILD_BCC_INSIDE_KATA = "true" ]; then
-    mkdir -p bcc/build
-    cd bcc/build
-    git checkout tags/v$BCC
-    cmake ..
-    make
+#if [ $BUILD_BCC_INSIDE_KATA = "true" ]; then
+#    mkdir -p bcc/build
+#    cd bcc/build
+#    git checkout tags/v$BCC
+#    cmake ..
+#    make
+#else
+cd bcc/build
+#    echo "BCC should have been built outside Kata"
+#fi
+if [ $STATIC = "true" ]; then
+    find . -name "*.a" -exec cp -v {} /usr/lib/ \;
+    find . -name "*.so" -exec cp -v {} /usr/lib/ \;
 else
-    cd bcc/build
-    echo "BCC should have been built outside Kata"
+    make install
 fi
-make install
-find . -name "*.a" -exec cp -v {} /usr/lib/ \;
 
 # for rust-bcc compiling
 export RUSTFLAGS="-L /usr/lib -L /usr/lib64 -L /usr/lib/x86_64-linux-gnu -L /usr/lib/llvm-$LLVM/lib"
 
 # Add cargo repo to speed up crate download
-tee $CARGO_HOME/config <<EOF
+if [ $ADD_CARGO_REPO_MIRROR = "true" ]; then
+    tee $CARGO_HOME/config <<EOF
 [source.crates-io]
 registry = "https://github.com/rust-lang/crates.io-index"
 replace-with = 'ustc'
 [source.ustc]
 registry = "git://mirrors.ustc.edu.cn/crates.io-index"
 EOF
+fi
 
 # Build and test rust-bcc
 cd $WORK_DIR
@@ -54,4 +63,4 @@ else
     #cargo test --features "${FEATURES} static llvm_${LLVM}"
 fi
 
-$WORK_DIR/target/debug/write
+timeout 20s $WORK_DIR/target/debug/write
